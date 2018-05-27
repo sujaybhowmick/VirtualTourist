3f7555732ed3c83df205951b7ecfd407625cc2a9
Sujay Bhowmick <sujay.bhowmick@sentifi.com>
2018-05-27 14:43:20 +0700

    Added PhotoAlbumViewController

---
+++ VirtualTourist/ViewControllers/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoViewController.swift
+//  VirtualTourist
+//
+//  Created by Sujay Bhowmick on 5/27/18.
+//  Copyright © 2018 Sujay Bhowmick. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
+    
+}
